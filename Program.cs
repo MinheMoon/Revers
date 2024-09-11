f67197aa472a20ae974727b28d903190e6036d1e
Mael <maeldream1@gmail.com>
2024-09-11 22:32:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class ArrayReverser
+{
+    public static int[] Reverse(int[] array)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left < right)
+        {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return array;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 5, 3 };
+        Console.WriteLine("Оригінальний масив: " + string.Join(", ", numbers));
+
+        ArrayReverser.Reverse(numbers);
+        Console.WriteLine("Обернений масив: " + string.Join(", ", numbers));
+    }
+}
